94030f3f811716c17293587b9f2db89f19af45be
Batterrang <batman@warplan.com>
2025-11-19 14:58:19 -0600

    Update

--- server.js
+++ server.js
@@ -153,4 +153,4 @@ app.post('/webhooks/ghl-to-zoom', async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port ${port}`));
